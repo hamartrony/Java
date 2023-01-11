daa0b8107194a9365f87751a4d03b3010bce1855
Hamart Rony Lucena <68301688+hamartrony@users.noreply.github.com>
2023-01-11 09:08:50 -0300

    upload files

---
+++ src/account/AccExceptions.java
@@ -0,0 +1,11 @@
+package account;
+
+public class AccExceptions extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+
+  public AccExceptions(String msg) {
+    super(msg);
+  }
+  
+}
